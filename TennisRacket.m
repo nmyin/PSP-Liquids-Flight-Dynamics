0629af6c9373c5c99a102b76aaace89581d5c99a
Nishka Yadav <85858597+nmyin@users.noreply.github.com>
2026-09-24 13:19:10 -0400

    TennisRacket
    Refactored code into a function for better structure and added initial state vector and simulation duration.

--- TennisRacket.m
+++ TennisRacket.m
@@ -1,8 +1,20 @@
-% Defining variables
-ix = 0.02; % Moment of Inertia along x axis in kg*m^2
-iy = 0.003; % Moment of Inertia along y axis in kg*m^2
-iz = 0.027; % Moment of Inertia along z axis in kg*m^2
-w = [pi;0.05;0.05]; % Initial angular velocity components in rad/s
-q0 = [1;0;0;0]; % Initial quaternion
-% Angular acceleration
-a = [(iy-iz)*w(2)*w(3)/ix; (iz-ix)*w(1)*w(3)/iy; (ix-iy)*w(1)*w(2)/iz];
+function TennisRacket()
+    % Defining variables
+    ix = 0.02; % Moment of Inertia along x axis in kg*m^2
+    iy = 0.003; % Moment of Inertia along y axis in kg*m^2
+    iz = 0.027; % Moment of Inertia along z axis in kg*m^2
+    w0 = [pi;0.05;0.05]; % Initial angular velocity components in rad/s
+    q0 = [1;0;0;0]; % Initial quaternion
+    x0 = [w0;q0]; % Initial state vector
+    tspan = linspace(0,10,100); % Duration of the simulation
+    
+    [t, x_f] = ode45(@my_ode, tspan, x0); 
+    
+    RotationsVisualizer(x_f(:, 4:7), t.', 1)
+    
+    function x_dot = my_ode(t, x)
+        w_dot = [(iy-iz)*x(2)*x(3)/ix; (iz-ix)*x(1)*x(3)/iy; (ix-iy)*x(1)*x(2)/iz];
+        q_dot = 1/2*[0 -1*x(1) -1*x(2) -1*x(3); x(1) 0 x(3) -1*x(2); x(2) -1*x(3) 0 x(1); x(3) x(2) -1*x(1) 0]*x(4:7);
+        x_dot = [w_dot;q_dot];
+    end
+end
